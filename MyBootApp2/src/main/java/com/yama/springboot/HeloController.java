3465b3f504bffc4b67a5ba095d07666a246eea95
yy05853 <c05853yy@gmail.com>
2019-10-06 13:53:35 +0900

    list 3-9

--- MyBootApp2/src/main/java/com/yama/springboot/HeloController.java
+++ MyBootApp2/src/main/java/com/yama/springboot/HeloController.java
@@ -1,14 +1,18 @@
 package com.yama.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello Spring-Boot world!!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int res = 0;
+		for(int i = 1; i <= num; i++)
+			res += i;
+		return "total: " + res;
 	}
 
 }
