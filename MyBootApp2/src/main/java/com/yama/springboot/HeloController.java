cb83ee954cf2ba0bd7c15d01ee1cac1696f653f7
yy05853 <c05853yy@gmail.com>
2019-10-06 16:15:21 +0900

    workReport

--- MyBootApp2/src/main/java/com/yama/springboot/HeloController.java
+++ MyBootApp2/src/main/java/com/yama/springboot/HeloController.java
@@ -1,18 +1,28 @@
 package com.yama.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num) {
-		int res = 0;
-		for(int i = 1; i <= num; i++)
-			res += i;
-		return "total: " + res;
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg","本日の出退勤時刻を記入してください");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str,@RequestParam("text2")String str2, ModelAndView mav) {
+		mav.addObject("msg","本日は" + str + "から" + str2 + "まで勤務しました");
+		mav.addObject("value",str);
+		mav.addObject("value2",str2);
+		mav.setViewName("index");
+		return mav;
 	}
 
 }
