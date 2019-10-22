9afaab7a1dca1f104a48de0646ed363c71a3de0a
yy05853 <c05853yy@gmail.com>
2019-10-22 17:04:34 +0900

    sample

--- MyBootApp2/src/main/java/com/yama/springboot/controller/SampleController.java
+++ MyBootApp2/src/main/java/com/yama/springboot/controller/SampleController.java
@@ -2,14 +2,12 @@ package com.yama.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.yama.springboot.dao.SampleDao;
-import com.yama.springboot.entity.Sample;
 
 @Controller
 public class SampleController {
@@ -18,23 +16,32 @@ public class SampleController {
     SampleDao repository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index(
-    		@ModelAttribute("formModel") Sample sample,
-    		ModelAndView mav) {
+    public ModelAndView index(ModelAndView mav) {
+
+    	mav.addObject("msg","商品の値段を入力してください");
     	mav.setViewName("index");
-    	mav.addObject("msg","this is sample content.");
-    	Iterable<Sample> list = repository.findAll();
-    	mav.addObject("datalist",list);
     	return mav;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    @Transactional(readOnly=false)
-    public ModelAndView form(
-    		@ModelAttribute("formModel") Sample sample,
+    public ModelAndView send(
+    		@RequestParam("text1")String str,
+    		@RequestParam(value="check1",required=false)boolean check1,
     		ModelAndView mav) {
-    	repository.saveAndFlush(sample);
-    	return new ModelAndView("redirect:/");
+
+
+    	int price = Integer.parseInt(str);
+    	int taxPrice;
+
+    	if(check1) {
+    		taxPrice = price * 108 / 100;
+    	}else {
+    		taxPrice = price * 110 / 100;
+    	}
+
+    	mav.addObject("msg", "税込" + taxPrice +"円です");
+    	mav.setViewName("index");
+    	return mav;
 
 	}
 }
