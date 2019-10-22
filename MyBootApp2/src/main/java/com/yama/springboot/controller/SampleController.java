136b4e823ed73e5e0eaaa36d1f3edcbd52e7a7fb
yy05853 <c05853yy@gmail.com>
2019-10-22 15:00:48 +0900

    sample

--- MyBootApp2/src/main/java/com/yama/springboot/controller/SampleController.java
+++ MyBootApp2/src/main/java/com/yama/springboot/controller/SampleController.java
@@ -1,26 +1,40 @@
 package com.yama.springboot.controller;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yama.springboot.dao.SampleDao;
 import com.yama.springboot.entity.Sample;
 
-@RestController
+@Controller
 public class SampleController {
 
     @Autowired
-    private SampleDao sampleDao;
+    SampleDao repository;
 
-    @RequestMapping(method = RequestMethod.GET)
-    @Transactional
-    public List<Sample> get() {
-        return sampleDao.findAll();
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(
+    		@ModelAttribute("formModel") Sample sample,
+    		ModelAndView mav) {
+    	mav.setViewName("index");
+    	mav.addObject("msg","this is sample content.");
+    	Iterable<Sample> list = repository.findAll();
+    	mav.addObject("datalist",list);
+    	return mav;
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly=false)
+    public ModelAndView form(
+    		@ModelAttribute("formModel") Sample sample,
+    		ModelAndView mav) {
+    	repository.saveAndFlush(sample);
+    	return new ModelAndView("redirect:/");
+
+	}
 }
